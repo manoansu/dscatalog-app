b91f046605d53c1f411a3c67f5920c8ff22e2dea
Ansumane Mané <manoansu@gmail.com>
2021-09-16 09:32:30 +0100

    DTO

--- backend/src/main/java/pt/amane/dscatalog/services/CategoryService.java
+++ backend/src/main/java/pt/amane/dscatalog/services/CategoryService.java
@@ -1,11 +1,13 @@
 package pt.amane.dscatalog.services;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import pt.amane.dscatalog.dtos.CategoryDTO;
 import pt.amane.dscatalog.entities.Category;
 import pt.amane.dscatalog.repositories.CategoryRepository;
 
@@ -17,8 +19,9 @@ public class CategoryService {
 	
 	
 	@Transactional(readOnly = true)
-	public List<Category> findAll(){
-		return repository.findAll();
+	public List<CategoryDTO> findAll(){
+		List<Category> list = repository.findAll();
+		return list.stream().map(dto -> new CategoryDTO(dto)).collect(Collectors.toList());
 	}
 
 }
